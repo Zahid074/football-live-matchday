b34cfb3e2b7937a85ff455555a0121397c4a6857
MD Zahid Hossain <mdzahedhasann@gmail.com>
2026-08-06 23:17:08 +0600

    Update Tailwind content paths for better matching

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ["./index.html", "./*.{js,jsx}", "./**/*.{js,jsx}"],
   darkMode: "class",
   theme: { extend: {} },
   plugins: [],
